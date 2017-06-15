bc344de2b5cbbf95613c7a700dabb758bec45a05
Diego Agüero <Diego_c-walker17@hotmail.com>
2017-06-15 19:20:55 -0400

    Versión 002
    se le agrego un JPanel al JFrame que contenía al grafico y también se agrego el botón volver con su funcionamiento para regresar a la ventana principal

--- src/main/java/view/graficoGasto.java
+++ src/main/java/view/graficoGasto.java
@@ -6,9 +6,14 @@
 package view;
 
 import data.Proceso;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -20,6 +25,8 @@ import org.jfree.data.category.DefaultCategoryDataset;
  * @author Equipo
  */
 public class graficoGasto extends javax.swing.JFrame{
+    private JPanel Panelgrafico;
+    private JButton volver;
     private Principal Vp;
     private Proceso pr;
     private int gasto[] = new int[12];
@@ -36,50 +43,72 @@ public class graficoGasto extends javax.swing.JFrame{
     }
 
     private void initComponent() {
-        this.addWindowListener(new WindowAdapter() {
-            public void WindowClosing(WindowEvent e){
-                Vp.setVisible(true);
-                graficoGasto.this.dispose();
+//        this.addWindowListener(new WindowAdapter() {
+//            public void WindowClosing(WindowEvent e){
+//                Vp.setVisible(true);
+//                graficoGasto.this.dispose();
+//            }
+//        });
+
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.HIDE_ON_CLOSE);
+        this.setTitle("Grafic");
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        this.Panelgrafico = new JPanel();
+        this.Panelgrafico.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(this.Panelgrafico);
+        this.Panelgrafico.setLayout(null);
+
+        this.volver = new JButton("Volver");
+        this.volver.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                volver();
             }
         });
+        this.volver.setBounds(430, 570, 89, 23);
+        Panelgrafico.add(this.volver);
         
         DefaultCategoryDataset data = new DefaultCategoryDataset();
         final String x = "gasto";
         
         
-        data.addValue(gasto[0], x, "enero");
-        data.addValue(gasto[1], x, "febrero");
-        data.addValue(gasto[2], x, "marzo");
-        data.addValue(gasto[3], x, "abril");
-        data.addValue(gasto[4], x, "mayo");
-        data.addValue(gasto[5], x, "junio");
-        data.addValue(gasto[6], x, "julio");
-        data.addValue(gasto[7], x, "agosto");
-        data.addValue(gasto[8], x, "septiembre");
-        data.addValue(gasto[9], x, "ocrubre");
-        data.addValue(gasto[10], x, "noviembre");
-        data.addValue(gasto[11], x, "diciembre");
+        data.addValue(gasto[0], x, "ene");
+        data.addValue(gasto[1], x, "feb");
+        data.addValue(gasto[2], x, "mar");
+        data.addValue(gasto[3], x, "abr");
+        data.addValue(gasto[4], x, "may");
+        data.addValue(gasto[5], x, "jun");
+        data.addValue(gasto[6], x, "jul");
+        data.addValue(gasto[7], x, "ago");
+        data.addValue(gasto[8], x, "sep");
+        data.addValue(gasto[9], x, "oct");
+        data.addValue(gasto[10], x, "nov");
+        data.addValue(gasto[11], x, "dic");
         
         
-        JFreeChart grafica = ChartFactory.createBarChart("Grafica gastos del año", "mes", "Dinero", data, PlotOrientation.VERTICAL, true, true, false);
+        JFreeChart grafica = ChartFactory.createBarChart("Grafica gastos", "mes", "Dinero", data, PlotOrientation.VERTICAL, true, true, false);
         
         ChartPanel contenedor = new ChartPanel(grafica);
         
-        JFrame ventana = new JFrame("Grafic expense");
-        ventana.add(contenedor);
-        ventana.setSize(900, 600);
-        ventana.setVisible(true);
-        ventana.setLocationRelativeTo(null);
+        contenedor.setBounds(15, 10, 900, 550);
+        this.Panelgrafico.add(contenedor);
+        
+        this.setSize(950, 650);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+//        JFrame ventana = new JFrame("Grafic expense");
+//        ventana.add(contenedor);
+//        ventana.setSize(900, 600);
+//        ventana.setVisible(true);
+//        ventana.setLocationRelativeTo(null);
                
     }
     
     public void sumaGastoMes (String mes, int m){
-        System.out.println("mes: "+ mes);
         for (int i = 0; i < pr.cantidadRegistro() ; i++) {
-            System.out.println("algo: "+pr.obtenerRegistro(i).getgFecha().split("-")[1]);
             if(mes.equals(pr.obtenerRegistro(i).getgFecha().split("-")[1]) ) { 
                 gasto[m] = Integer.valueOf(pr.obtenerRegistro(i).getgGasto())+ gasto[m]; 
-                System.out.println("gasto: "+ gasto[m] );
             }
         }
         
@@ -97,6 +126,12 @@ public class graficoGasto extends javax.swing.JFrame{
             sumaGastoMes(mes, i);
             
         }
+    }
+    
+    public void volver(){
+        this.dispose();
+        Vp.setVisible(true);
+        
     }
     
     
