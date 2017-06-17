250143e492accf2f6b3c6a2ef55f4a7b50af8395
Diego Agüero <Diego_c-walker17@hotmail.com>
2017-06-16 21:55:53 -0400

    version 004
    Se realizaron unos cambios mínimos en el grafico

--- src/main/java/view/graficoGasto.java
+++ src/main/java/view/graficoGasto.java
@@ -1,17 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 import data.Proceso;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import org.jfree.chart.ChartFactory;
@@ -20,17 +16,16 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
-/**
- *
- * @author Equipo
- */
-public class graficoGasto extends javax.swing.JFrame{
+public class graficoGasto extends JFrame {
+
     private JPanel Panelgrafico;
-    private JButton volver;
+    private JButton btnVolver;
     private Principal Vp;
     private Proceso pr;
     private int gasto[] = new int[12];
-    
+    private JMenuBar jmbMenu;
+    private JMenu jmVolver;
+
     JFreeChart grafica;
     DefaultCategoryDataset datos = new DefaultCategoryDataset();
 
@@ -39,40 +34,35 @@ public class graficoGasto extends javax.swing.JFrame{
         conteoMes();
         this.Vp = vp;
         initComponent();
-       
+
     }
 
     private void initComponent() {
-//        this.addWindowListener(new WindowAdapter() {
-//            public void WindowClosing(WindowEvent e){
-//                Vp.setVisible(true);
-//                graficoGasto.this.dispose();
-//            }
-//        });
 
-        this.setDefaultCloseOperation(javax.swing.WindowConstants.HIDE_ON_CLOSE);
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         this.setTitle("Grafic");
-        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
         this.Panelgrafico = new JPanel();
         this.Panelgrafico.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(this.Panelgrafico);
         this.Panelgrafico.setLayout(null);
 
-        this.volver = new JButton("Volver");
-        this.volver.addMouseListener(new MouseAdapter() {
+        jmbMenu = new JMenuBar();
+        jmVolver = new JMenu();
+        jmVolver.setToolTipText("volver");
+        jmVolver.setIcon(new ImageIcon("imagenes\\back2.png"));
+        jmVolver.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 volver();
             }
         });
-        this.volver.setBounds(430, 570, 89, 23);
-        Panelgrafico.add(this.volver);
-        
+        jmbMenu.add(jmVolver);
+        setJMenuBar(jmbMenu);
+
         DefaultCategoryDataset data = new DefaultCategoryDataset();
         final String x = "gasto";
-        
-        
+
         data.addValue(gasto[0], x, "ene");
         data.addValue(gasto[1], x, "feb");
         data.addValue(gasto[2], x, "mar");
@@ -85,55 +75,47 @@ public class graficoGasto extends javax.swing.JFrame{
         data.addValue(gasto[9], x, "oct");
         data.addValue(gasto[10], x, "nov");
         data.addValue(gasto[11], x, "dic");
-        
-        
-        JFreeChart grafica = ChartFactory.createBarChart("Grafica gastos", "mes", "Dinero", data, PlotOrientation.VERTICAL, true, true, false);
-        
-        ChartPanel contenedor = new ChartPanel(grafica);
-        
-        contenedor.setBounds(15, 10, 900, 550);
+
+        JFreeChart jfcGrafica = ChartFactory.createBarChart("Grafica gastos", "mes", "Dinero", data, PlotOrientation.VERTICAL, true, true, false);
+
+        ChartPanel contenedor = new ChartPanel(jfcGrafica);
+
+        contenedor.setBounds(15, 10, 900, 560);
         this.Panelgrafico.add(contenedor);
-        
-        this.setSize(950, 650);
+
+        this.setSize(940, 660);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-//        JFrame ventana = new JFrame("Grafic expense");
-//        ventana.add(contenedor);
-//        ventana.setSize(900, 600);
-//        ventana.setVisible(true);
-//        ventana.setLocationRelativeTo(null);
-               
+
     }
-    
-    public void sumaGastoMes (String mes, int m){
-        for (int i = 0; i < pr.cantidadRegistro() ; i++) {
-            if(mes.equals(pr.obtenerRegistro(i).getgFecha().split("-")[1]) ) { 
-                gasto[m] = Integer.valueOf(pr.obtenerRegistro(i).getgGasto())+ gasto[m]; 
+
+    public void sumaGastoMes(String mes, int m) {
+        for (int i = 0; i < pr.cantidadRegistro(); i++) {
+            if (mes.equals(pr.obtenerRegistro(i).getgFecha().split("-")[1])) {
+                gasto[m] = Integer.valueOf(pr.obtenerRegistro(i).getgGasto()) + gasto[m];
             }
         }
-        
+
     }
-    
-    public void conteoMes (){
-        
+
+    public void conteoMes() {
+
         for (int i = 0; i < 12; i++) {
             String mes = null;
-            if (String.valueOf(i+1).length() == 1) {
-                mes = "0"+String.valueOf(i+1);
-            }else{
-                mes = String.valueOf(i+1);
+            if (String.valueOf(i + 1).length() == 1) {
+                mes = "0" + String.valueOf(i + 1);
+            } else {
+                mes = String.valueOf(i + 1);
             }
             sumaGastoMes(mes, i);
-            
+
         }
     }
-    
-    public void volver(){
+
+    public void volver() {
         this.dispose();
         Vp.setVisible(true);
-        
+
     }
-    
-    
-    
+
 }
